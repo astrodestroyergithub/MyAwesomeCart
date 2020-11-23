a40df01f6fc7bf87d4eeb39c09f599650555d94c
AstroDestroyer146 <astrodestroyer146@gmail.com>
2020-11-23 18:28:15 +0530

    Added media directory

--- mac/settings.py
+++ mac/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -121,4 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Managing media
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
